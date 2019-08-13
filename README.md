b77a6afde640bafc7b1e374e1da11863b45927d7
Henrik Lund Kramshoej <hlk@kramse.org>
2019-08-13 20:53:45 +0200

    First packet

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # frankenpacket
 What is the most encapsulated packet, with a somewhat believable story
+
+
+So internet uses Internet Protocol (IP) suite but over the years a lot of technologies have been added to this mess :-D
+
+Lets try to do some encapsulation, and lets see what stupid, strange, fun packets we can generate!
+
+## Rules
+
+Be Excellent to Each Other
+
+Produce frankenpackets with a LOT of encapsulation and layers!
+
+Produce packets in a repeatable way, I choose Scapy https://scapy.readthedocs.io/en/latest/installation.html which is easy to work with.
+
+Packets should have a somewhat believable story, so adding 1000 (one thousand) MPLS labels is not believable.
+
+So if you add IPv6 extension headers, I guess the limit is somewhere 5-10
+
+First example in hlk-mpls-vxlan-datacenter
+ is an example which might happen if an ISP uses Ethernet, MPLS, VLAN and the customer uses VXLAN - to send a DNS packet, which is UDP, IP, in Ethernet.
+
+Please include the pcap also when submitting packets.
+
+## Goals
+
+Have fun and learn networking on a low level.
+
+Find packets that make routers and firewalls puke, or at least spend more resources processing.
+
+I will use these as input for Zeek https://www.zeek.org/ and Suricata https://suricata-ids.org/ to see if they break :-D
---
+++ hlk-mpls-vxlan-datacenter/mpls-vxlan-datacenter.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+#
+
+# This frankenpacket uses multiple levels of encapsulation.
+
+# Its not unrealistic to see one such if two datacenters uses VXLAN
+# and the underlying internet provider uses MPLS
+
+###[ Loading modules ]###
+import sys
+import getopt
+#from scapy.all import PcapReader, wrpcap, Packet, NoPayload
+from scapy.all import *
+
+load_contrib("mpls")
+mpls_eth = Ether(src="00:16:3e:11:11:11", dst="ca:01:07:fc:00:1c", type=0x8847)
+mpls_lables=MPLS(label=16, s=0, ttl=255)/MPLS(label=18, s=0, ttl=255)/MPLS(label=18, s=0, ttl=255)/MPLS(label=16, s=1, ttl=255)
+
+# Add GRE
+prepacket=mpls_eth/mpls_lables/Ether(dst="00:00:00:00:00:03")/Dot1Q(vlan=42)
+
+vtepsrc="192.0.2.1"
+vtepdst="192.0.2.2"
+vxlanport=4789
+vni=100
+
+# Create a VXLAN header
+vxlan=prepacket/IP(src=vtepsrc,dst=vtepdst)/UDP(sport=1234,dport=vxlanport)/VXLAN(vni=vni,flags="Instance")
+
+# Make VXLAN innner packet
+broadcast="ff:ff:ff:ff:ff:ff"
+srcmac="00:24:9b:47:0d:49"
+source="198.51.100.124"
+dstmac="00:50:56:12:34:56"
+destination="198.51.100.200"
+
+realpacket=IP(src=source,dst=destination)/UDP()/DNS(rd=1,id=0xdead,qd=DNSQR(qname="www.bornhack.dk"))
+
+packet=vxlan/Ether(dst=dstmac,src=srcmac)/realpacket
+#packet2=vxlan/Ether(dst=dstmac,src=srcmac)/packet
+
+# Debug
+#packet.show()
+
+headers=len(packet)-len(realpacket)
+
+# Stats
+print "Length of packet with all encapsulation: " + str(len(packet))
+print "Length of headers packet: " + str(headers)
+print "Length of innermost packet: " + str(len(realpacket))
+print "Overhead ratio: " + str(100 * len(packet) / float ( len(realpacket)) )
+
+wrpcap("packet.cap",packet)
+wireshark (packet)
